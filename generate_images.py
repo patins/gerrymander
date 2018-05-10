39be5b18526aabf6971dc1226ffb6a0e7a44febc
Patrick Insinger <patrickinsinger@gmail.com>
2018-05-10 18:38:41 -0400

    Add district images

--- generate_images.py
+++ generate_images.py
@@ -101,6 +101,51 @@ def plot(file_name):
     plt.ylim(ymin=min_y, ymax=max_y)
     plt.axis('off')
 
+def plot_districts(file):
+    state = file.split('/')[1]
+    _, fips = build_state_adjacency_matrix(state)
+    voter_data = build_voter_data(fips)
+    populations = voter_data.sum(axis=0)
+
+    labeling = np.load(file)
+    assert labeling.shape[0] <= 20
+    
+    all_points = np.concatenate([county_points[f] for f in fips])
+    max_x, max_y = all_points.max(axis=0)
+    min_x, min_y = all_points.min(axis=0)
+    max_y, min_y = 90*np.sin(np.pi/180*np.array([max_y, min_y]))
+    mid_x = (min_x + max_x)/2
+    mid_y = (min_y + max_y)/2
+    diff_x = (max_x - min_x)/2
+    diff_y = (max_y - min_y)/2
+    diff = max(diff_x, diff_y)
+    min_x, max_x = mid_x - diff, mid_x + diff
+    min_y, max_y = mid_y - diff, mid_y + diff
+    
+    color_map = plt.get_cmap('tab20')
+    
+    fig, ax = plt.subplots()
+    fig.set_size_inches((7,7))
+
+    for district in range(labeling.shape[0]):
+        for county in labeling[district, :].nonzero()[0]:
+            f = fips[county]
+            points = county_points[f]
+            start = np.where((points == points[-1]).all(axis=1))[0][0]
+            first_points = points[start:].copy()
+            first_points[:, 1] = 90*np.sin(np.pi/180*first_points[:, 1])
+            ax.add_patch(Polygon(
+                first_points,
+                True,
+                color=color_map(district),
+                ec=(0,0,0,0.1)
+            ))    
+
+    plt.xlim(xmin=min_x, xmax=max_x)
+    plt.ylim(ymin=min_y, ymax=max_y)
+    plt.axis('off')
+
+
 for fn in glob.glob('runs/*/*.npy'):
     img_name = 'imgs/' + fn[5:-4] + '.png'
     plot(fn)
@@ -108,4 +153,12 @@ for fn in glob.glob('runs/*/*.npy'):
     if not os.path.exists(path):
         os.mkdir(path)
     plt.savefig(img_name, dpi=300)
-    print("Generated image for %s" % fn)
+
+    img_name = 'district_imgs/' + fn[5:-4] + '.png'
+    plot_districts(fn)
+    path = 'district_imgs/' + fn[5:].split('/')[0]
+    if not os.path.exists(path):
+        os.mkdir(path)
+    plt.savefig(img_name, dpi=300)
+
+    print("Generated images for %s" % fn)
